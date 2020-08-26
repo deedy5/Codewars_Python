0cd0d511da8ddf34e69bacfbac16704d9efc82c5
deedy5 <65482418+deedy5@users.noreply.github.com>
2020-08-26 16:34:29 +0100

    Create Build Tower.py

---
+++ 6kyu/Build Tower.py
@@ -0,0 +1,42 @@
+'''
+Build Tower
+
+Build Tower by the following given argument:
+number of floors (integer and always greater than 0).
+
+Tower block is represented as *
+
+    Python: return a list;
+    JavaScript: returns an Array;
+    C#: returns a string[];
+    PHP: returns an array;
+    C++: returns a vector<string>;
+    Haskell: returns a [String];
+    Ruby: returns an Array;
+    Lua: returns a Table;
+
+Have fun!
+
+for example, a tower of 3 floors looks like below
+
+[
+  '  *  ', 
+  ' *** ', 
+  '*****'
+]
+
+and a tower of 6 floors looks like below
+
+[
+  '     *     ', 
+  '    ***    ', 
+  '   *****   ', 
+  '  *******  ', 
+  ' ********* ', 
+  '***********'
+]
+'''
+
+def tower_builder(n_floors):
+    width = n_floors * 2 - 1
+    return [('*' * ((i+1)*2-1) ).center(width) for i in range(n_floors)]
