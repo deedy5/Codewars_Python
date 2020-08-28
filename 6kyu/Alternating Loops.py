aa1eb92cdf002a9538d5c82616d61bd55461f77a
deedy5 <65482418+deedy5@users.noreply.github.com>
2020-08-28 20:52:33 +0000

    Create Alternating Loops.py

---
+++ 6kyu/Alternating Loops.py
@@ -0,0 +1,19 @@
+'''
+Write
+
+function combine()
+
+that combines arrays by alternatingly taking elements passed to it.
+
+E.g
+
+combine(['a', 'b', 'c'], [1, 2, 3]) == ['a', 1, 'b', 2, 'c', 3]
+combine(['a', 'b', 'c'], [1, 2, 3, 4, 5]) == ['a', 1, 'b', 2, 'c', 3, 4, 5]
+combine(['a', 'b', 'c'], [1, 2, 3, 4, 5], [6, 7], [8]) == ['a', 1, 6, 8, 'b', 2, 7, 'c', 3, 4, 5]
+
+Arrays can have different lengths.
+'''
+
+from itertools import zip_longest
+def combine(*args):
+    return [y for x in zip_longest(*args) for y in list(x) if y is not None]
