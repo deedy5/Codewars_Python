3028c743dc2c528073e6ca88342bc6b6ccf04a86
deedy5 <65482418+deedy5@users.noreply.github.com>
2020-09-06 20:09:39 +0100

    Update Reverse polish notation calculator.py

--- 6kyu/Reverse polish notation calculator.py
+++ 6kyu/Reverse polish notation calculator.py
@@ -12,6 +12,8 @@ Valid operations are +, -, *, /.
 You may assume that there won't be exceptional situations (like stack underflow or division by zero).
 '''
 
+'''
+#v1
 def calc(expr):    
     exprlist = expr.split()
     while exprlist:
@@ -30,3 +32,18 @@ def calc(expr):
             if len(exprlist) == 1:
                 return float(exprlist[0])
     return 0
+'''
+
+#v2
+import operator
+def calc(expr):
+    operators = {'+': operator.add,'-': operator.sub,'*': operator.mul,'/': operator.truediv}
+    cache = [0]
+    exprlist = expr.split()
+    for x in exprlist:
+        if x in operators:
+            b, a = cache.pop(), cache.pop()
+            cache.append(operators[x](a, b))
+        elif x:
+            cache.append(float(x))
+    return cache.pop()
