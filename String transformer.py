0a9f875a03f22432b21c530f0a0e55b2172bde3e
deedy5 <65482418+deedy5@users.noreply.github.com>
2020-10-03 12:02:46 +0000

    Create String transformer.py

---
+++ String transformer.py
@@ -0,0 +1,20 @@
+'''
+Given a string, return a new string that has transformed based on the input:
+
+    Change case of every character, ie. lower case to upper case, upper case to lower case.
+    Reverse the order of words from the input.
+
+Note: You will have to handle multiple spaces, and leading/trailing spaces.
+
+For example:
+
+"Example Input" ==> "iNPUT eXAMPLE"
+
+You may assume the input only contain English alphabet and spaces.
+'''
+
+
+def string_transformer(s):
+    s = s.split(' ')
+    s.reverse()
+    return ' '.join(x.swapcase() for x in s)
